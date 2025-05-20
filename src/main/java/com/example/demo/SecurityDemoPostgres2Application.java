cbba6aa2cbf3512f1a764dd8d2016d5cd37f4caf
Victor Armenta <armenta_victor@outlook.com>
2025-05-19 22:07:12 -0600

    second commit

---
+++ src/main/java/com/example/demo/SecurityDemoPostgres2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoPostgres2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoPostgres2Application.class, args);
+	}
+
+}
